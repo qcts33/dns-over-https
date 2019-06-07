6286f7a4aa8e577ddaef97c0201cf574fd88d323
qcts33 <qcthreestones@gmail.com>
2019-06-07 23:02:36 +0800

    add test

---
+++ test_doh.py
@@ -0,0 +1,23 @@
+import aiohttp
+import pytest
+
+from doh import fetch_json
+
+
+@pytest.mark.asyncio
+async def test_fetch_json():
+    headers = {"accept": "application/dns-json"}
+    params = {"name": "example.com", "type": "A"}
+    timeout = aiohttp.ClientTimeout(sock_connect=5)
+    url = "dns.233py.com/dns-query"
+    async with aiohttp.ClientSession(headers=headers, timeout=timeout) as session:
+        u, ans = await fetch_json(url, params, session)
+    for a in ans["Answer"]:
+        if isinstance(a, dict):
+            if a["type"] == 1:
+                answer = a["data"]
+                break
+        else:
+            answer = a
+            break
+    assert answer == "93.184.216.34"
