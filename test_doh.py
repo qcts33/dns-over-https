8c8aaad8118451ac1370745e571b0ccc433e3433
qcts33 <qcthreestones@gmail.com>
2019-06-09 00:08:47 +0800

    add test fetch_wireformat

--- test_doh.py
+++ test_doh.py
@@ -1,7 +1,8 @@
 import aiohttp
 import pytest
+import dns.message
 
-from doh import fetch_json
+from doh import fetch_json, fetch_wireformat
 
 
 @pytest.mark.asyncio
@@ -20,4 +21,22 @@ async def test_fetch_json():
         else:
             answer = a
             break
+    assert answer == "93.184.216.34"
+
+
+@pytest.mark.asyncio
+async def test_fetch_wireformat():
+    query = dns.message.make_query(qname="example.com", rdtype="A").to_wire()
+    headers = {
+        "accept": "application/dns-message",
+        "content-type": "application/dns-message",
+    }
+    timeout = aiohttp.ClientTimeout(sock_connect=5)
+    url = "dns.233py.com/dns-query"
+    async with aiohttp.ClientSession(headers=headers, timeout=timeout) as session:
+        u, ans = await fetch_wireformat(url=url, query=query, session=session)
+    for a in ans.answer:
+        if a.rdtype == 1:
+            data = a.items
+            answer = data[0].to_text()
     assert answer == "93.184.216.34"
