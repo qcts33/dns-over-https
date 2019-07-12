dfb96c57563e02f5849ddac5ab83336c0dc41bcd
qcts33 <qcthreestones@gmail.com>
2019-07-12 22:03:52 +0800

    fix test

--- test_doh.py
+++ test_doh.py
@@ -1,8 +1,8 @@
 import aiohttp
-import pytest
 import dns.message
+import pytest
 
-from doh import fetch_json, fetch_wireformat
+from doh import fetch_json, fetch_wireformat, get_trace_config
 
 
 @pytest.mark.asyncio
@@ -10,9 +10,12 @@ async def test_fetch_json():
     headers = {"accept": "application/dns-json"}
     query = {"name": "example.com", "type": "A"}
     timeout = aiohttp.ClientTimeout(sock_connect=5)
-    url = "dns.233py.com/dns-query"
-    async with aiohttp.ClientSession(headers=headers, timeout=timeout) as session:
-        u, ans = await fetch_json(url=url, query=query, session=session)
+    url = "dns.cloudflare.com"
+    trace_config = get_trace_config()
+    async with aiohttp.ClientSession(
+        headers=headers, timeout=timeout, trace_configs=[trace_config]
+    ) as session:
+        u, ans, elapsed = await fetch_json(url=url, query=query, session=session)
     for a in ans["Answer"]:
         if isinstance(a, dict):
             if a["type"] == 1:
@@ -32,9 +35,14 @@ async def test_fetch_wireformat():
         "content-type": "application/dns-message",
     }
     timeout = aiohttp.ClientTimeout(sock_connect=5)
-    url = "dns.233py.com/dns-query"
-    async with aiohttp.ClientSession(headers=headers, timeout=timeout) as session:
-        u, ans = await fetch_wireformat(url=url, query=query, session=session)
+    url = "dns.233py.com"
+    trace_config = get_trace_config()
+    async with aiohttp.ClientSession(
+        headers=headers, timeout=timeout, trace_configs=[trace_config]
+    ) as session:
+        u, ans, elapsed = await fetch_wireformat(
+            url=url, query=query, session=session, proxy=None
+        )
     for a in ans.answer:
         if a.rdtype == 1:
             data = a.items
